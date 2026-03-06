0342bc591b6535110e640aea88fbdcf004edbe72
Poons-L <106232124+Poons-L@users.noreply.github.com>
2026-03-06 18:19:49 +0800

    Updated NewApp.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,12 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import NewApp from './NewApp.tsx';
 import { AuthProvider } from './contexts/AuthContext';
-import { NavigationProvider } from './contexts/NavigationContext';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
-      <NavigationProvider>
-        <NewApp />
-      </NavigationProvider>
+      <NewApp />
     </AuthProvider>
   </StrictMode>
 );
